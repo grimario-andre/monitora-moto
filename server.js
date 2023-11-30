d46a19967d3ff8cd2063aec8e2824c72ab90e8b4
grimario-andre <andre02souza@outlook.com.br>
2023-11-29 21:26:40 -0300

    Settings database

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const sequelize = require('./src/database/databaseConnection');
 
 // Use a variável de ambiente SERVER_PORT, se disponível. Caso contrário, use 3000 como padrão.
 const port = process.env.SERVER_PORT;
@@ -14,3 +15,11 @@ app.use(express.json());
 app.listen(port, () => {
   console.log(`Aplicação rodando em http://localhost:${port}`);
 });
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conexão estabelecida com sucesso.');
+  })
+  .catch(err => {
+    console.error('Não foi possível conectar ao banco de dados:', err);
+  });
